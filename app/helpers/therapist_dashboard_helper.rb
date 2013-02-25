e99ad77264a8bb6bedb3a70ef531604a4b47bee3
James Hwang <jameshwang@outlook.com>
2013-02-25 13:46:00 -0500

    linked AWS S3 to heroku

--- app/helpers/therapist_dashboard_helper.rb
+++ app/helpers/therapist_dashboard_helper.rb
@@ -1,2 +1,22 @@
 module TherapistDashboardHelper
+
+	def retrieveAllRecordings
+		require File.expand_path(File.dirname(__FILE__)+'/../../aws_config')
+
+		# Hard-coded bucket_name. Must be changed later to address multiple patients
+		bucket_name = "recordings77"
+
+		# get ian instance of the S3 interface using the default configuration
+		s3=AWS::S3.new
+
+		# create a bucket
+		b = s3.buckets.create(bucket_name)
+		html = "<ul>"
+		# retrieve the filenames of all recordings
+		b.objects.each do |obj|
+			html << "<li>" + obj.key + "</li>"
+		end
+		html<<"</ul>"
+		html.html_safe		
+	end
 end
