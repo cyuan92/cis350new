495f8895bf6ae332f6556ef6e4e926860753bf29
James Hwang <jameshwang@outlook.com>
2013-03-09 16:24:04 -0500

    first attempt to isolate individual patient

--- app/helpers/therapist_dashboard_helper.rb
+++ app/helpers/therapist_dashboard_helper.rb
@@ -1,5 +1,32 @@
 module TherapistDashboardHelper	
 
+	def retrieveAllPatients
+		require File.expand_path(File.dirname(__FILE__)+'/../../aws_config')
+
+		# Hard-coded bucket_name. Must be changed later to address multiple patients
+		bucket_name = "recordings77"
+		therapist_name = "conyuan"
+		patient_name = "wingyli"
+		# get an instance of the S3 interface using the default configuration
+		s3=AWS::S3.new
+
+		# create a bucket
+		b = s3.buckets.create(bucket_name)
+		html="<h1>"+patient_name+"\'s recordings</h1>\n"		
+		# retrieve the filenames of all recordings
+		#html <<"<ul class=\"recordings "+ patient_name + "\">"
+		b.objects.each do |obj|			
+			if obj.key.match(therapist_name+"\/+\w") 				
+
+				html << "\n<li>"
+				html << obj.key
+				html <<"</li>\n"
+			end			
+		end
+		html<<"</ul>"
+		html.html_safe		
+	end
+
 	def retrieveAllRecordings
 		require File.expand_path(File.dirname(__FILE__)+'/../../aws_config')
 
